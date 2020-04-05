52689c092ec1d70d872903adfb939dab9257be5b
nicolettemiller <nicolette.annabelle@gmail.com>
2020-04-05 00:41:43 -0700

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
 	has_many :ideas
 	validates :name, presence: true
 	validates :email, presence: true
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
 end
