d1bf15b13680ed49d962e5124b737c77716d1771
nicolettemiller <nicolette.annabelle@gmail.com>
2020-04-05 00:32:40 -0700

    Added 'bcrypt' to Gemfile to hash user passwords. Added attributes to user model and ran tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
 	has_many :ideas
+	validates :name, presence: true
+	validates :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_024202) do
+ActiveRecord::Schema.define(version: 2020_04_05_072756) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_024202) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
