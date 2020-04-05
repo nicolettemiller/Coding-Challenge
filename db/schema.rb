95d0d79ad2a7eb8cbfe84a46695c052b8afb3aa1
nicolettemiller <nicolette.annabelle@gmail.com>
2020-04-05 11:45:52 -0700

    added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_072756) do
+ActiveRecord::Schema.define(version: 2020_04_05_184012) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_05_072756) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
